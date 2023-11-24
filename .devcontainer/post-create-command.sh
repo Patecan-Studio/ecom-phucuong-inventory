0b166931108872df75d142df70ac708e7d59a10f
minhnguyenduy99 <44431867+minhnguyenduy99@users.noreply.github.com>
2023-11-24 08:01:18 +0000

    Update devcontainer

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,18 +1,16 @@
-# Install nvm
-# set root user to run script
 NODE_VERSION=18.11.0
-curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# config git
+git config --global core.autocrlf true
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# load nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm -v
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install $NODE_VERSION
 nvm alias default $NODE_VERSION
 nvm use default
-
-# config git
-git config --global user.name 'Nguyen Duy Minh'
-git config --global user.email 'minhnguyenduy99@gmail.com'
 
 # install package
 npm install
